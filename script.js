8e77652caff92fb0e8970d70ddee421a8beed696
jennifer coral <jenyferp@hotmail.com>
2021-10-12 17:55:56 +0200

    primer desafio

---
+++ script.js
@@ -0,0 +1,7 @@
+let nombre  =  prompt("Cual es el nombre de tu mascota: ")
+let raza = prompt("Cual es la raza  de tu mascota: ")
+let edad1 = Number(prompt("cuantos años tiene tu mascota: "))
+let edad2= Number(prompt("cual es tu edad: "))
+let suma = edad1 + edad2
+alert ("el nombre y raza son:  " + nombre + " " + raza )
+alert ("tu edad mas la de tu mascosta :"  + suma)
